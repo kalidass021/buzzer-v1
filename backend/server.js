1de450b5c59834166d98101e93679aef5cce9177
Kalidass <kalidass.k021@gmail.com>
2024-09-23 22:42:58 +0530

    implement error handling middleware and add body parser middleware

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,25 @@ dotenv.config();
 // port
 const PORT = process.env.PORT || 5000;
 
+// allow json as input to the backend
+// body parser
+app.use(express.json()); // to parse req.body
+app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
+
 app.use('/api/auth/', authRoutes);
 
+// middleware to handle the errors
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is up and listening on port ${PORT}`);
-    connectMongoDB();
+  console.log(`Server is up and listening on port ${PORT}`);
+  connectMongoDB();
 });
