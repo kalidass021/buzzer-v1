5edb8f2d268c6d7236e0e428cd615f8abb0518b6
Kalidass <kalidass.k021@gmail.com>
2024-09-25 22:00:21 +0530

    implement comment on post route

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -13,7 +13,7 @@ export const createPost = async (req, res, next) => {
     if (!user) {
       return next(errorHandler(404, 'User not found'));
     }
-    
+
     if (!text && !img) {
       return next(errorHandler(400, 'Post must have text or image'));
     }
@@ -35,4 +35,60 @@ export const createPost = async (req, res, next) => {
     console.error(`Error in createPost ${err.message}`);
     next(err);
   }
+};
+
+export const deletePost = async (req, res, next) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return next(errorHandler(404, 'Post not found'));
+    }
+
+    if (post.user.toString() !== req.user._id.toString()) {
+      return next(
+        errorHandler(401, 'You are not authorized to delete this post')
+      );
+    }
+
+    if (post.img) {
+      const imgId = post.img.split('/').pop().split('.')[0];
+      await cloudinary.uploader.destroy(imgId);
+    }
+
+    await Post.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({ message: 'Post deleted successfully' });
+  } catch (err) {
+    console.error(`Error in deletePost ${err.message}`);
+    next(err);
+  }
+};
+
+export const commentOnPost = async (req, res, next) => {
+    try {
+        const {text} = req.body;
+        const postId = req.params.id;
+        const userId = req.user._id;
+
+        if (!text) {
+            return next(errorHandler(400, 'Text field is required'));
+        }
+
+        const post = await Post.findById(postId);
+        if (!post) {
+            return next(errorHandler(404, 'Post not found'));
+        }
+        
+        const comment = {user: userId, text};
+
+        post.comments.push(comment);
+
+        await post.save();
+
+        res.status(200).json(post);
+
+    } catch (err) {
+        console.error(`Error in commentOnPost ${err.message}`);
+        next(err);
+    }
 };
--- backend/routes/post.route.js
+++ backend/routes/post.route.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.js';
-import { createPost } from '../controllers/post.controller.js';
+import { createPost, deletePost, commentOnPost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post('/create', protectRoute, createPost);
 // router.post('/like/:id', protectRoute, likeUnlikePost);
-// router.post('/comment/:id', protectRoute, commentOnPost);
-// router.delete('/delete/:id', protectRoute, deletePost);
+router.post('/comment/:id', protectRoute, commentOnPost);
+router.delete('/:id', protectRoute, deletePost);
 
 export default router;
 
