b9d8c5f59f8d87efc0be91796d1a963bbf09e5fc
Kalidass <kalidass.k021@gmail.com>
2024-09-27 11:52:59 +0530

    config daisy ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div className='text-3xl font-bold underline'>Hello, World!</div>;
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>Hello, World!</div>
+    </>
+  );
 };
 
 export default App;
