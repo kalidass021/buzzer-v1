9ca151978cbd1f98906a460bb8ffce26c41df60d
Kalidass <kalidass.k021@gmail.com>
2024-09-27 09:30:43 +0530

    implement deleteNotifications route

--- backend/controllers/notification.controller.js
+++ backend/controllers/notification.controller.js
@@ -1,19 +1,59 @@
-import Notification from "../models/notification.model.js";
+import Notification from '../models/notification.model.js';
+import { errorHandler } from '../lib/utils/errorHandler.js';
 
 export const getNotifications = async (req, res, next) => {
-    try {
-        const userId = req.user._id;
+  try {
+    const userId = req.user._id;
 
-        const notifications = await Notification.find({to: userId}).populate({
-            path: 'from',
-            select: 'username profileImg'
-        });
+    const notifications = await Notification.find({ to: userId }).populate({
+      path: 'from',
+      select: 'username profileImg',
+    });
 
-        await Notification.updateMany({to: userId}, {read: true});
+    await Notification.updateMany({ to: userId }, { read: true });
 
-        res.status(200).json(notifications);
-    } catch (err) {
-        console.error(`Error in getNotifications ${err.message}`);
-        next(err);
-    }
+    res.status(200).json(notifications);
+  } catch (err) {
+    console.error(`Error in getNotifications ${err.message}`);
+    next(err);
+  }
 };
+
+// delete all notifications
+export const deleteNotifications = async (req, res, next) => {
+  try {
+    const userId = req.user._id;
+
+    await Notification.deleteMany({ to: userId });
+
+    res.status(200).json({ message: 'Notifications deleted successfully' });
+  } catch (err) {
+    console.error(`Error in deleteNotifications ${err.message}`);
+    next(err);
+  }
+};
+
+// delete one notification
+// export const deleteNotification = async (req, res, next) => {
+//     try {
+//         const notificationId = req.params.id;
+//         const userId = req.user._id;
+        
+//         const notification = await Notification.findById(notificationId);
+
+//         if (!notification) {
+//             return next(errorHandler(404, 'Notification not found'));
+//         }
+
+//         if (notification.to.toString() !== userId.toString()) {
+//             return next(errorHandler(402, "You're not allowed to delete this notification"));
+//         }
+
+//         await Notification.findByIdAndDelete(notificationId);
+
+//         res.status(200).json({message: 'Notification deleted successfully'});
+//     } catch (err) {
+//         console.error(`Error in deleteNotification ${err.message}`);
+//         next(err);
+//     }
+// };
--- backend/routes/notification.route.js
+++ backend/routes/notification.route.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { getNotifications } from '../controllers/notification.controller.js';
+import {
+  getNotifications,
+  deleteNotifications,
+//   deleteNotification,
+} from '../controllers/notification.controller.js';
 import { protectRoute } from '../middlewares/protectRoute.js';
 
 const router = express.Router();
 
 router.get('/', protectRoute, getNotifications);
-// router.delete('/', protectRoute, deleteNotifications);
+router.delete('/', protectRoute, deleteNotifications);
+// router.delete('/:id', protectRoute, deleteNotification);
 
 export default router;
